511bb7aefa8c18a725c2178dca41594f7c38b4de
Tiffany <274891081+tiff-m-tech@users.noreply.github.com>
2026-04-13 12:06:44 -0400

    Finished Problem Exercises.

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,49 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
+// Original Code
+// let result = "5" - 2;
+// console.log("The result is: " + result); // The result is 3
 
-let result = "5" - 2;
+// let isValid = Boolean("false");
+// if (isValid) {
+//     console.log("This is valid!"); // This is valid!
+// }
+
+// let age = "25";
+// let totalAge = age + 5;
+// console.log("Total Age: " + totalAge); // Total Age: 255
+
+
+// PART 1
+// Change to number for clarity.
+let result = Number("5") - 2;
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+// Assuming this is suppose to be false? Converting with Boolean() just gave a value so it was true.
+let isValid = false;
 if (isValid) {
     console.log("This is valid!");
 }
 
-let age = "25";
+// Change to number for clarity. A string concatenated to a number will just add the number at the end of a string.
+let age = Number("25");
 let totalAge = age + 5;
-console.log("Total Age: " + totalAge);
+console.log("Total Age: " + totalAge); // Total Age: 25
+
+// PART 2
+// Implicit example
+let total = "5" * 2;
+console.log(total); // 10
+
+// Explicit example
+let myAge = "25";
+console.log(myAge) // "25"
+let convertedAge = Number(myAge);
+console.log(convertedAge) // 25
+
+// Edge Case
+let notANumber = "Hello";
+let convertedNum = Number(notANumber);
+console.log(convertedNum) // NaN
+console.log(typeof(convertedNum)) // number
